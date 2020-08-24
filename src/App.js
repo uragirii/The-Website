413a1c165b258e8883ed6af0c976d49456f8ea19
Apoorv Kansal <apoorvkansalak@gmail.com>
2020-08-25 02:39:37 +0530

    🃏Added 404 page

--- src/App.js
+++ src/App.js
@@ -1,49 +1,19 @@
 import React from 'react';
 import './App.css';
-import { Heading, Button, Para, TextInput, TextArea, ProjectsBar } from "./components/index";
 import ContactMe from "./ContactMe"
 import Projects from "./Projects"
 import Home from "./Home"
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import E404 from './404'
+import { Route, Switch } from 'react-router-dom';
 
-
-const styles = {
-  "section": {
-    "height": "100vh",
-    "maxHeight": "100vh",
-
-    // "borderBottom": "1px solid #000000",
-
-  },
-  "heading" : {
-    "fontFamily": "Montserrat",
-    "fontStyle": "normal",
-    "fontWeight": "bold",
-    "fontSize": "36px",
-    "lineHeight": "44px",
-    "letterSpacing": "0.07em",
-    "color": "#000000",
-    "textAlign" : "center",
-  },
-  "caption" : {
-    "fontFamily": "Consolas",
-    "fontStyle": "normal",
-    "fontWeight": "bold",
-    "fontSize": "18px",
-    "lineHeight": "21px",
-    "color": "rgba(0, 0, 0, 0.5)",
-    "textAlign" : "center",
-    "marginTop" : "3%"
-  }
-  
-}
 function App(){
   return (
     <div className="App">
       <Switch>
         <Route path="/" component={Home} exact/> 
         <Route path="/contact" component={ContactMe} exact/> 
-        <Route path="/projects" component={Projects} exact/> 
+        <Route path="/projects" component={Projects} exact/>
+        <Route component={E404} /> 
       </Switch>
     </div>
   )
