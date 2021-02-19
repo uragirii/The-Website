909349be34e496b8aa7f997f0313c0fd89ed4614
Apoorv Kansal <apoorvkansalak@gmail.com>
2021-02-20 00:38:58 +0530

    Update LobbyVerify.js

--- src/components/LobbyVerify.js
+++ src/components/LobbyVerify.js
@@ -27,7 +27,7 @@ export default function LobbyVerify({saveMovie, code, link,setSubs}) {
     const [movie, setMovie ]  = useState(null)
     const [verified, setVerified] = useState(false)
     const file = useRef(null)
-    const size = 132730740
+    const size = 158296306
 
     const checkAndProceed = (selectedFile, subs)=>{
         if(selectedFile.size == size){
